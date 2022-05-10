5c7884e15ec19662817bc51cd8e9daaf26fbc23d
Auroresifflet <aurore.sifflet@gmail.com>
2022-05-10 17:46:02 +0200

    Voici mon dernier exercice du jour

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      {/* partie1 */}
+      <View style={{ flex: 1, flexDirection: "row" }}>
+        <View style={{ flex: 3, flexDirection: "column" }}>
+          <View style={{ flex: 3, backgroundColor: "purple" }} />
+          <View style={{ flex: 1, backgroundColor: "yellow" }} />
+          <View style={{ flex: 1, backgroundColor: "pink" }} />
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 4, backgroundColor: "red" }} />
+          <View style={{ flex: 1, backgroundColor: "blue" }} />
+        </View>
+        <View style={{ flex: 1, flexDirection: "column" }}>
+          <View style={{ flex: 4, backgroundColor: "lightblue" }} />
+          <View style={{ flex: 2, backgroundColor: "fuchsia" }} />
+        </View>
+      </View>
+      {/* partie2 */}
+      <View style={{ flex: 1 }}>
+        <View style={{ flex: 3, backgroundColor: "gray" }} />
+      </View>
     </View>
   );
 }
@@ -13,8 +31,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
